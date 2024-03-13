6422931ef527c494f26527de7456cb0c0fca49a1
licvv2115 <najki291@gmail.com>
2024-03-13 01:10:03 +0100

    initial view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,202 @@
+
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <body>
+    <header class="bg-2_color py-4 border-b border-1_color shadow-2xl">
+  <div class="container mx-auto flex justify-between items-center">
+    <!-- Logo and Happenings Around -->
+    <div class="flex justify-start items-center space-x-8">
+      <!-- Logo -->
+      <!-- Happenings Around -->
+      <a href="#" class="text-white text-2xl font-mono font-bold ">placeholder</a>
+    </div>
+
+    <!-- Navigation and Sign Up to the right -->
+    <div class="flex justify-end items-center space-x-8">
+      <!-- Navigation -->
+      <nav class="hidden md:flex space-x-8">
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Home</a>
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Events</a>
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">About us</a>
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Profile</a>
+
+      </nav>
+
+      <!-- Sign Up button -->
+      <a href="#" class="text-white font-mono text-xl  bg-1_color font-medium py-2 px-4 rounded transition-transform duration-300 transform hover:scale-110">Sign Up</a>
+      
+    </div>
   </div>
+</header>
+<section id='mission-items' class="w-full h-24 ">
+<div class=" container mx-auto flex justify-center items-center">
+<p class="text-black text-2xl font-mono font-bold py-5">Nasza misja</p>
+</div>
+<div class=" container mx-auto flex justify-center items-center">
+<p class="text-gray-700 text-xl font-mono font-bold py-1">opis opis opisopis opis opisopis opis opisopis opis opisopis opis opisopis opis opis</p>
+</div>
+</section>
+<section class="w-full py-5">
+  <div class="container mx-auto flex justify-between flex-wrap">
+
+    <div class="w-1/5 p-4 text-center">
+      <div class="bg-3_color h-96 p-6 rounded-lg shadow-2xl flex flex-col justify-around items-center">
+        <!-- Tytuł misji -->
+        <p class="text-black text-xl font-mono">Tytuł misji</p>
+        <!-- Opis misji -->
+        <p class="text-black text-sm font-mono">Opis misji</p>
+        <!-- Miejsce na obrazek -->
+        <div class="w-full h-40 bg-gray-200 flex justify-center items-center">
+          <img src="ścieżka_do_obrazka" alt="Obrazek misji" class="max-h-full max-w-full">
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/5 p-4 text-center">
+      <div class="bg-3_color h-96 p-6 rounded-lg shadow-2xl flex flex-col justify-around items-center">
+        <!-- Tytuł misji -->
+        <p class="text-black text-xl font-mono">Tytuł misji</p>
+        <!-- Opis misji -->
+        <p class="text-black text-sm font-mono">Opis misji</p>
+        <!-- Miejsce na obrazek -->
+        <div class="w-full h-40 bg-gray-200 flex justify-center items-center">
+          <img src="ścieżka_do_obrazka" alt="Obrazek misji" class="max-h-full max-w-full">
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/5 p-4 text-center">
+      <div class="bg-3_color h-96 p-6 rounded-lg shadow-2xl flex flex-col justify-around items-center">
+        <!-- Tytuł misji -->
+        <p class="text-black text-xl font-mono">Tytuł misji</p>
+        <!-- Opis misji -->
+        <p class="text-black text-sm font-mono">Opis misji</p>
+        <!-- Miejsce na obrazek -->
+        <div class="w-full h-40 bg-gray-200 flex justify-center items-center">
+          <img src="ścieżka_do_obrazka" alt="Obrazek misji" class="max-h-full max-w-full">
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/5 p-4 text-center">
+      <div class="bg-3_color h-96 p-6 rounded-lg shadow-2xl flex flex-col justify-around items-center">
+        <!-- Tytuł misji -->
+        <p class="text-black text-xl font-mono">Tytuł misji</p>
+        <!-- Opis misji -->
+        <p class="text-black text-sm font-mono">Opis misji</p>
+        <!-- Miejsce na obrazek -->
+        <div class="w-full h-40 bg-gray-200 flex justify-center items-center">
+          <img src="ścieżka_do_obrazka" alt="Obrazek misji" class="max-h-full max-w-full">
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/5 p-4 text-center">
+      <div class="bg-3_color h-96 p-6 rounded-lg shadow-2xl flex flex-col justify-around items-center">
+        <!-- Tytuł misji -->
+        <p class="text-black text-xl font-mono">Tytuł misji</p>
+        <!-- Opis misji -->
+        <p class="text-black text-sm font-mono">Opis misji</p>
+        <!-- Miejsce na obrazek -->
+        <div class="w-full h-40 bg-gray-200 flex justify-center items-center">
+          <img src="ścieżka_do_obrazka" alt="Obrazek misji" class="max-h-full max-w-full">
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section>
+<section class="w-full py-5">
+  <div class="container mx-auto text-center">
+    <h2 class="text-black text-2xl font-mono font-bold py-5">Autorzy</h2>
+  </div>
+  <div class=" container mx-auto flex justify-center items-center">
+<p class="text-gray-700 text-xl font-mono font-bold py-1">opis opis opisopis opis opisopis opis opisopis opis opisopis opis opisopis opis opis</p>
+</div>
+</section>
+
+<section class="w-full py-5">
+  <div class="container mx-auto flex justify-around items-center flex-wrap">
+    <!-- Autor 1 -->
+    <div class="w-1/2 md:w-1/4 p-4 flex flex-col items-center">
+
+      <div class="w-64 h-64 mb-3 rounded-full overflow-hidden">
+        <img src="../assets/images/photo1.jpg" alt="Imię i nazwisko autora 1" class="w-full h-full object-cover">
+      </div>
+      <h3 class="text-black text-xl font-bold">Piotrek ...</h3>
+      <p class="text-black text-m mt-2">Krótki opis autora 1.</p>
+    </div>
+    <!-- Autor 2 -->
+    <div class="w-1/2 md:w-1/4 p-4 flex flex-col items-center">
+
+      <div class="w-64 h-64 mb-3 rounded-full overflow-hidden">
+        <img src="../assets/images/photo2.jpg" alt="Imię i nazwisko autora 2" class="w-full h-full object-cover">
+      </div>
+      <h3 class="text-black text-xl font-bold">Patryk ...</h3>
+      <p class="text-black text-m mt-2">Krótki opis autora 2.</p>
+    </div>
+  </div>
+</section>
+<section class="py-10 ">
+  <div class="container mx-auto text-center">
+    <h2 class="text-3xl font-bold mb-6">Następna sekcja</h2>
+    <p class="text-lg text-gray-700 max-w-2xl mx-auto mb-8">opsi opsi opsi opisopsi opsi opsi opisopsi opsi opsi opisopsi opsi opsi opisopsi opsi opsi opisopsi opsi opsi opisopsi opsi opsi opis.</p>
+  </div>
+
+  <div class="container mx-auto  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-2">
+    <!-- Case Study 1 -->
+    <div ref="caseStudy1" class="case-study-container bg-3_color rounded-lg shadow-xl p-6 flex flex-col justify-between">
+      <h3 class="font-bold text-xl mb-3">Tytuł 1</h3>
+      <p class="text-gray-600 short-text ">Krótki opis...</p>
+      <p class="text-gray-600 full-text hidden">Pełny opis case study, wraz ze szczegółami o projekcie, procesie pracy i osiągniętych wynikach...</p>
+      <button @click="toggleText('caseStudy1')" class="mt-4 inline-block bg-2_color text-black py-2 px-4 rounded transition-colors read-more">Czytaj więcej</button>
+    </div>
+    <!-- Case Study 2 -->
+    <div ref="caseStudy2" class="case-study-container bg-3_color rounded-lg shadow-xl p-6 flex flex-col justify-between">
+      <h3 class="font-bold text-xl mb-3">Tytuł 1</h3>
+      <p class="text-gray-600 short-text">Krótki opis...</p>
+      <p class="text-gray-600 full-text hidden">Pełny opis case study, wraz ze szczegółami o projekcie, procesie pracy i osiągniętych wynikach...</p>
+      <button @click="toggleText('caseStudy2')" class="mt-4 inline-block bg-2_color text-black py-2 px-4 rounded transition-colors read-more">Czytaj więcej</button>
+    </div>
+    <!-- Case Study 3 -->
+    <div ref="caseStudy3" class="case-study-container bg-3_color rounded-lg shadow-xl p-6 flex flex-col justify-between">
+      <h3 class="font-bold text-xl mb-3">Tytuł 1</h3>
+      <p class="text-gray-600 short-text">Krótki opis...</p>
+      <p class="text-gray-600 full-text hidden">Pełny opis case study, wraz ze szczegółami o projekcie, procesie pracy i osiągniętych wynikach...</p>
+      <button @click="toggleText('caseStudy3')" class="mt-4 inline-block bg-2_color text-black py-2 px-4 rounded transition-colors read-more">Czytaj więcej</button>
+    </div>
+  </div>
+</section>
+<section class="w-full py-5">
+  <div class="container mx-auto text-center">
+
+  </div>
+</section>
+</body>
 </template>
+
+
+<script>
+export default {
+  name: 'ToggleText',
+  methods: {
+    toggleText(refName) {
+      const caseStudy = this.$refs[refName];
+      const fullText = caseStudy.querySelector('.full-text');
+      const shortText = caseStudy.querySelector('.short-text');
+      const readMoreButton = caseStudy.querySelector('.read-more');
+
+      if (fullText.classList.contains('hidden')) {
+        fullText.classList.remove('hidden');
+        shortText.classList.add('hidden');
+        readMoreButton.textContent = 'Czytaj mniej';
+      } else {
+        fullText.classList.add('hidden');
+        shortText.classList.remove('hidden');
+        readMoreButton.textContent = 'Czytaj więcej';
+      }
+    },
+  },
+}
+</script>
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
   content: [
-    './src/**/*.{vue,js,ts,jsx,tsx}'
+    "./src/**/*.{vue,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '1_color' : '#8E7AB5',
+        '2_color' : '#B784B7',
+        '3_color':'#E493B3',
+        '4_color':'#EEA5A6',
+        'font_color':'#5E1675',
+      },
+      fontFamily: {
+        sans: ['Modimi One', 'sans-serif'],
+      },
+      backgroundColor: {
+        'default': '#8E7AB5',
+        'sample':'#EEA5A6',
+      },
+    },
   },
   plugins: [],
 }
