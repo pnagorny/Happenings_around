c7313f82191b8893856be95a6f7a8fc0fbaa90ca
licvv2115 <najki291@gmail.com>
2024-03-13 12:04:29 +0100

    header home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,35 @@
 <template>
-  <h1>Hejka</h1>
+  <body>
+  <header class="bg-2_color py-8  shadow-2xl">
+  <div class="container mx-auto flex justify-between items-center">
+    <!-- Logo and Happenings Around -->
+    <div class="flex justify-start items-center space-x-8">
+      <!-- Logo -->
+      <!-- Happenings Around -->
+      <a href="#" class="text-white text-2xl font-mono font-bold ">placeholder</a>
+    </div>
+
+    <!-- Navigation and Sign Up to the right -->
+    <div class="flex justify-end items-center space-x-8">
+      <!-- Navigation -->
+      <nav class="hidden md:flex space-x-8">
+        <button @click="$router.push('/')" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Home</button>
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Events</a>
+        <button @click="$router.push('about')" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">About us</button>
+        <a href="#" class="text-white font-mono text-xl hover: hover:bg-1_color hover:rounded-lg px-4 py-1 transition-all">Profile</a>
+
+      </nav>
+
+      <!-- Sign Up button -->
+      <a href="#" class="text-white font-mono text-xl  bg-1_color font-medium py-2 px-4 rounded transition-transform duration-300 transform hover:scale-110">Sign Up</a>
+      
+    </div>
+  </div>
+ 
+
+</header>
+<section>
+<h1>Hejka</h1>
   <div>
     <div>
       <button @click="$router.push('about')">About</button>
@@ -15,6 +45,9 @@
     </div>
     
 </div>
+</section>
+</body>
+  
 </template>
 
 <script>
