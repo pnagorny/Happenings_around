03640be6e5a2bf316b3c7ff9d00ff73c4d17d9ca
Piotr Nagorny <pnagorny05@outlook.com>
2024-04-09 13:57:38 +0200

    side panel bug fix

--- src/components/HeaderNotLoggedIn.vue
+++ src/components/HeaderNotLoggedIn.vue
@@ -2,11 +2,12 @@
   <header class="bg-2_color py-8  shadow-2xl">
 <div class="container mx-auto md:flex justify-between items-center">
   <div class="w-full text-lg font-extrabold md:w-fit md:text-left">
-      <span class="text-violet-500 text-2xl ml-14 md:ml-0">Happenings</span>
-      <span class="text-white text-2xl ">Around</span>
-      <button @click="MenuOpen()" class="md:hidden text-white">
-        <svg class="ml-16  w-8 h-8 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+    <button @click="MenuOpen()" class="md:hidden text-white">
+        <svg class="ml-4 w-8 h-8 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
       </button>
+      <span class="text-violet-500 text-3xl ml-6 md:ml-0">Happenings</span>
+      <span class="text-white text-3xl">Around</span>
+      
     </div>
   <div class="md:flex md:justify-end md:items-center">
     
