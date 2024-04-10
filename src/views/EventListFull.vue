cb8b9f369257e38adfbe7cc746ea43676ad7112b
licvv2115 <najki291@gmail.com>
2024-04-10 23:42:35 +0200

    some adjustments

--- src/views/EventListFull.vue
+++ src/views/EventListFull.vue
@@ -10,33 +10,38 @@
 </form>
   </div>
 </section>
-<section class="text-gray-600 body-font">
+<section class= "font-sans">
 <div class="container px-5 py-24 mx-auto">
   <div class="flex flex-wrap -m-4">
-    <div class="p-4 md:w-1/3" v-for="event in events" :key="event.id">
-      <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-2_color">
-        <img class="lg:h-80 md:h-36 w-full object-cover object-center" :src="event.imageURL || 'https://dummyimage.com/720x600'" alt="event image">
+    <div class="p-4  md:w-1/3 sm:w-full rounded-2xl hover:shadow-2xl hover:-translate-y-1 transition-all duration-300 relative" v-for="event in events" :key="event.id">
+      <a href="" class="hover:text-red absolute  top-5 right-5 mt-2 mr-3">
+        <svg class="w-10 h-10 text-slate-100 hover:text-red-500 duration-300" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
+            </svg>
+        </a>
+      <div class="h-full rounded-2xl overflow-hidden bg-slate-100">
+        <img class="lg:h-80 md:h-36 sm:h-80 w-full  object-cover object-center" :src="event.imageURL || 'https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_QL75_UX380_CR0,1,380,562_.jpg'" alt="event image">
         <div class='p-6'>
-          <h2 class="text-l text-white mb-4 text-end">{{ event.eventDateTime }}</h2>
-          <h1 class="text-2xl font-bold text-white mb-3 text-center">{{ event.eventName }}</h1>
-          <h2 class="text-l text-bold text-white mb-3">{{ event.address }}</h2>
+          <h1 class="text-3xl font-bold text-black mb-3 line-clamp-2">{{ event.eventName }}</h1>
+          <h2 class="text-slate-400 pt-2 font-semibold ">{{ event.eventDateTime }}</h2>
+          <h2 class="text-slate-400 pt-2 font-semibold">{{ event.address }}</h2>
           <p :class="contentClass(event.id)">{{ event.eventDescription }}</p>
-          <button @click="toggle(event.id)" class="text-white underline decoration-solid text-lg font-bold inline-flex items-center md:mb-2 lg:mb-0">
-            {{ buttonText(event.id) }}
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="white" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M5 12h14"></path>
-      <path d="M12 5l7 7-7 7"></path>
-      </svg>
-      </button>
+          
+            <div class=" w-1/2">
+              <button><div class="group text-black hover:text-sky-600 transition underline decoration-solid ease-in-out duration-200">Czytaj więcej<span aria-hidden="true" class="inline-block translate-x-0 group-hover:translate-x-1 transition-transform ease-in-out duration-200">→</span></div></button>
+            </div>
+            
+          
+
       <div class="text-end">
-      <span class="text-white mr-3 inline-flex items-center text-end lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-      <svg class="w-4 h-4 mr-1" stroke="white" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+      <span class=" mr-3 inline-flex items-center text-end lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+      <svg class="w-4 h-4 mr-1" stroke="black" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
       <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
       <circle cx="12" cy="12" r="3"></circle>
       </svg>1.2K
     </span>
-    <span class="text-white inline-flex items-center leading-none text-sm">
-      <svg class="w-4 h-4 mr-1" stroke="white" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+    <span class=" inline-flex items-center leading-none text-sm">
+      <svg class="w-4 h-4 mr-1" stroke="black" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
       </svg>6
     </span>
@@ -146,15 +151,21 @@ export default {
 
     const contentClass = (id) => {
       return [
-        'leading-relaxed text-lg text-black mb-3 transition-max-height duration-500 ease-out overflow-hidden',
+        'leading-relaxed line-clamp-4 py-2 text-sm font-light mb-3 transition-max-height duration-500 ease-out overflow-hidden',
         expandedEvents[id] ? 'max-h-[1000px]' : 'max-h-72'
-      ];
+      ].join(' ');
     };
 
     const toggle = (id) => {
-      expandedEvents[id] = !expandedEvents[id];
+      if (expandedEvents[id]) {
+        expandedEvents[id] = false;
+      } else {
+        for (let key in expandedEvents) {
+          expandedEvents[key] = false;  // zwija wszystkie inne divy
+        }
+        expandedEvents[id] = true;  // rozwija tylko kliknięty div
+      }
     };
-
     return {
       events: visibleEvents,
       isLoggedIn,
@@ -166,7 +177,8 @@ export default {
       filter,
       toggle,
       buttonText,
-      contentClass
+      contentClass,
+      expandedEvents
     };
   }
 }
