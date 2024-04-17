ef06fe557d5fac8e77f7c23194c7fb36eec13197
Piotr Nagorny <pnagorny05@outlook.com>
2024-04-17 19:06:33 +0200

    final bugfix

--- src/views/EventListFull.vue
+++ src/views/EventListFull.vue
@@ -70,7 +70,7 @@ peer-checked:opacity-100 peer-checked: [&>*]:translate-y-0 peer-checked:[&>*]:sc
   
     <div  class="p-4 md:w-1/3 sm:w-full rounded-2xl hover:-translate-y-2 duration-300 relative" v-for="event in events" :key="event.id"  @click="openModal(event);handleZoomToEvent(event)" >
       <button @click.stop="toggleLikeEvent(event)" class="absolute top-5 right-5 mt-2 mr-3 focus:outline-none">
-    <svg :class="{ 'text-red-500': isEventLiked(event.id), 'text-slate-100': !isEventLiked(event.id) }" class="w-10 h-10 hover:text-red-500" fill="currentColor" viewBox="0 0 24 24">
+    <svg :class="{ 'text-red-500': isEventLiked(event.id), 'text-slate-100': !isEventLiked(event.id) }" class="w-10 h-10 hover:text-red-500 duration-300" fill="currentColor" viewBox="0 0 24 24">
       <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
     </svg>
   </button>
